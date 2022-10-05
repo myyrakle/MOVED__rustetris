faf1dc8b016194131888e65a1008eeac66130caf
myyrakle <sssang97@naver.com>
2022-10-06 01:23:57 +0900

    add: request_animation_frame 구현

---
+++ src/js_bind/request_animation_frame.rs
@@ -0,0 +1,5 @@
+fn request_animation_frame(f: &Closure<dyn FnMut()>) {
+    window()
+        .request_animation_frame(f.as_ref().unchecked_ref())
+        .expect("should register `requestAnimationFrame` OK");
+}
