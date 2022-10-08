583cfc1fd38e577e64991f062d109a9f28309d83
myyrakle <sssang97@naver.com>
2022-10-08 20:57:23 +0900

    add: keycode 매칭 구현

--- src/components/gamebox.rs
+++ src/components/gamebox.rs
@@ -252,8 +252,42 @@ impl Component for Model {
     fn view(&self, ctx: &Context<Self>) -> Html {
         let link = ctx.link();
 
+        let onkeydown = Callback::from(move |event: KeyboardEvent| {
+            match event.key_code() {
+                37 => {
+                    log::info!("left")
+                } // left move
+                39 => {
+                    log::info!("right")
+                } // right move
+                38 => {
+                    log::info!("up")
+                } // up move
+                40 => {
+                    log::info!("down")
+                } // down move
+                90 => {
+                    log::info!("z")
+                } // z
+                88 => {
+                    log::info!("x")
+                } // x
+                65 => {
+                    log::info!("a")
+                } // a
+                32 => {
+                    log::info!("space")
+                } // spacebar
+                16 => {
+                    log::info!("shift")
+                } // shift
+                _ => {}
+            }
+            // get the pressed key using web-sys out of the event
+        });
+
         html! {
-            <span>
+            <span {onkeydown}>
                 <canvas id="gamebox" width="300" height="600"></canvas>
                 <button onclick={link.callback(|_| Msg::GameStart)}>{"Start"}</button>
             </span>
