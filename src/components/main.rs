bd0193c9db154c4d9b6a47266482b24fcda8f42f
myyrakle <sssang97@naver.com>
2022-10-08 22:06:39 +0900

    remove: 불필요한 코드 제거

--- src/components/main.rs
+++ src/components/main.rs
@@ -1,16 +1,5 @@
-#![allow(clippy::let_unit_value)]
-use std::sync::{Arc, Mutex};
-
 use crate::components::gamebox;
-use yew::prelude::*;
 
-pub enum Msg {
-    AddOne,
-}
-
-pub struct Model {
-    value: Arc<Mutex<i64>>,
-}
 use yew::{function_component, html};
 
 #[function_component(MainComponent)]
