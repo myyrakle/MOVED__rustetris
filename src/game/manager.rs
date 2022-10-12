d6fb9c9400d1e58a0c0bb426e2702e605124aa59
myyrakle <sssang97@naver.com>
2022-10-13 01:30:50 +0900

    add: 홀드 렌더링 구현

--- src/game/manager.rs
+++ src/game/manager.rs
@@ -140,6 +140,8 @@ impl GameManager {
 
                     let next = game_info.bag.iter().map(|e| e.mino.into()).collect();
                     wasm_bind::render_next(next, 120, 520, 6, 26);
+
+                    wasm_bind::render_hold(game_info.hold.map(|e| e.mino.into()), 120, 120, 6, 6);
                 }
             });
 
