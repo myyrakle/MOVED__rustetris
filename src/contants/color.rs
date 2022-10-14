4e9b5301962d26d6c90b1c3fa10ddf5b2b28ee11
myyrakle <sssang97@naver.com>
2022-10-15 04:27:05 +0900

    change: 보드 색상 조정, 고스트 색상 추가

--- src/contants/color.rs
+++ src/contants/color.rs
@@ -1,4 +1,4 @@
-pub const BOARD_DEFAULT_COLOR: &str = "#D3D3D3";
+pub const BOARD_DEFAULT_COLOR: &str = "#eeeef1";
 pub const BOARD_STROKE_DEFAULT_COLOR: &str = "#000000";
 
 pub const NEXT_DEFAULT_COLOR: &str = "#212121";
@@ -6,3 +6,5 @@ pub const NEXT_STROKE_DEFAULT_COLOR: &str = "#000000";
 
 pub const HOLD_DEFAULT_COLOR: &str = "#212121";
 pub const HOLD_STROKE_DEFAULT_COLOR: &str = "#000000";
+
+pub const GHOST_DEFAULT_COLOR: &str = "#d3d3d3";
